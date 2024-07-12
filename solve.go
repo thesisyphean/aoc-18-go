d0fbb56e6d09f8c6082b86f189aa5b9b8755d140
Ethan Wolff <thesisyphean256@gmail.com>
2024-07-12 14:58:23 +0200

    Added first scaffold

--- solve.go
+++ solve.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Solving!")
+
+	dat, _ := os.ReadFile("/input/input1.txt")
+	lines := strings.Split(string(dat), "\n")
+	fmt.Println(solve1(lines))
 }
