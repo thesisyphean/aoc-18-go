5286fccd91affab2725f849af05660ee23462e0b
TheSisyphean <thesisyphean256@gmail.com>
2024-07-22 11:22:26 +0200

    Used flag to get the filename

--- solve.go
+++ solve.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide the location of the input file as the first argument.")
+	pathPtr := flag.String("path", "", "The path to the input file from input/")
+	flag.Parse()
+
+	if *pathPtr == "" {
+		fmt.Println("Please provide the path to the input file from input/ as a flag.")
 		return
 	}
 
-	dat, err := os.ReadFile(os.Args[1])
+	dat, err := os.ReadFile(fmt.Sprintf("input/%s", *pathPtr))
 
 	if err != nil {
-		fmt.Printf("Failed to read the input file. Did you download it to %s?", os.Args[1])
+		fmt.Printf("Failed to read the input file. Did you download it to %s?", *pathPtr)
 		return
 	}
 
-	lines := strings.Split(string(dat), "\n")
-	fmt.Println(solve1(lines))
+	lines := strings.Split(strings.TrimSpace(string(dat)), "\n")
+	fmt.Println(solve2(lines))
 }
